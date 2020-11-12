a601c6f6935f976cec352d1cb0a06f07792dd2f2
shinehtetoo <40478643@live.napier.ac.uk>
2020-11-12 17:23:48 +0630

    Add city.java file

---
+++ src/main/java/com/napier/sem/city.java
@@ -0,0 +1,9 @@
+package com.napier.sem;
+
+public class city {
+    public int ID;
+    public String Name;
+    public String CountryCode;
+    public String District;
+    public int Population;
+}
